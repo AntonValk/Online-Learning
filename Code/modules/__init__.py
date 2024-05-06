a0d93be215cd552dca55dd4a3e4ee9d30a5456ee
root <antonios.valkanas@mail.mcgill.ca>
2024-05-06 14:45:59 +0000

    forgot to add __init__ file

--- Code/modules/__init__.py
+++ Code/modules/__init__.py
@@ -1,8 +1,8 @@
-from .residual import ODLSetSingleStageResidualNet, SetSingleStageResidualNet, SingleStageResidualNet, AuxDrop_OGD, SingleStageResidualNetODL, Fast_AuxDrop_ODL, SetDecoder, MLP, StackofExperts, StackofExperts2
+from .residual import ODLSetSingleStageResidualNet, SetSingleStageResidualNet, SingleStageResidualNet, AuxDrop_OGD, SingleStageResidualNetODL, Fast_AuxDrop_ODL, SetDecoder, MLP, StackofExperts, StackofExperts2, KalmanMLPproto
 from .custom_layers import FCBlock, LayerNorm, FCBlockNorm, Embedding
 
 __all__ = [
     'ODLSetSingleStageResidualNet', 'SetSingleStageResidualNet', 'SingleStageResidualNet', 'AuxDrop_OGD', 'SingleStageResidualNetODL', 
     'Fast_AuxDrop_ODL', 'SetDecoder', 
-    'FCBlock', 'LayerNorm', 'FCBlockNorm', 'Embedding', 'MLP', 'StackofExperts', 'StackofExperts2'
+    'FCBlock', 'LayerNorm', 'FCBlockNorm', 'Embedding', 'MLP', 'StackofExperts', 'StackofExperts2', 'KalmanMLPproto'
 ]
